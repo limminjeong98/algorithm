430ea39ea063c058e8a1653f253e066df7f89218
HKMC\E183999 <limminjeong98@hyundai-autoever.com>
2021-09-08 14:55:19 +0900

    shortest path algorithm

---
+++ floyd-warshall.py
@@ -0,0 +1,24 @@
+INF = int(1e9)
+
+n, m = map(int, input().split())
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for k in range(1, n+1):
+    for a in range(1, n+1):
+        for b in range(1, n+1):
+            graph[a][b] = min(graph[a][k] + graph[k][b], graph[a][b])
+
+for a in range(1, n+1):
+    for b in range(1, n+1):
+        if graph[a][b] == INF:
+            print("INFINITY", end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
